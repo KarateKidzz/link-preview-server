1d80f5193f82462f6c13638b7db5f7fc3e8a1568
James Kelly <jameskelly21699@gmail.com>
2022-02-13 19:46:57 +0000

    Init commit

---
+++ link-preview-server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+var cors = require('cors');
+const port = 3000;
+const linkPreviewGenerator = require("link-preview-generator");
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+});
+
+app.get('/preview', async (req, res) => {
+    console.log('Received requst for preview');
+    try {
+        const previewData = await linkPreviewGenerator(req.query.url);
+        console.log(previewData);
+
+        res.json(previewData);
+      } catch (error) {
+          res.send(error);
+      }
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+});
