7cc8f1e4ea484a13a7bb05c6dbaaead2291db713
James Kelly <jameskelly21699@gmail.com>
2022-02-16 21:56:08 +0000

    Fix spelling mistake

--- link-preview-server.js
+++ link-preview-server.js
@@ -7,7 +7,7 @@ const linkPreviewGenerator = require("link-preview-generator");
 app.use(cors());
 
 app.get('/preview', async (req, res) => {
-    console.log('Generating preview for: ' + res.query.url);
+    console.log('Generating preview for: ' + req.query.url);
     try {
         const previewData = await linkPreviewGenerator(req.query.url, [
           '--single-process', '--no-zygote', '--no-sandbox'
