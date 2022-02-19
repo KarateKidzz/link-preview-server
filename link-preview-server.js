a7d532cc087e2656ea41e63a5e65874ae51312ea
James Kelly <jameskelly21699@gmail.com>
2022-02-19 10:29:53 +0000

    Do link scarping myself to save on resources

--- link-preview-server.js
+++ link-preview-server.js
@@ -2,23 +2,301 @@ const express = require('express');
 const app = express();
 var cors = require('cors');
 const port = 8081;
-const linkPreviewGenerator = require("link-preview-generator");
+const puppeteer = require("puppeteer-extra");
+const pluginStealth = require("puppeteer-extra-plugin-stealth");
+const util = require("util");
+const request = util.promisify(require("request"));
+const getUrls = require("get-urls");
+const isBase64 = require("is-base64");
+const LRU = require('lru-cache')
 
 app.use(cors());
 
-app.get('/preview', async (req, res) => {
-    console.log('Generating preview for: ' + req.query.url);
-    try {
-        const previewData = await linkPreviewGenerator(req.query.url, [
-          '--single-process', '--no-zygote', '--no-sandbox'
-        ]);
-        res.json(previewData);
-      } catch (error) {
-	        console.log(error);
-          res.send(error);
+const minimal_args = [
+  '--autoplay-policy=user-gesture-required',
+  '--disable-background-networking',
+  '--disable-background-timer-throttling',
+  '--disable-backgrounding-occluded-windows',
+  '--disable-breakpad',
+  '--disable-client-side-phishing-detection',
+  '--disable-component-update',
+  '--disable-default-apps',
+  '--disable-dev-shm-usage',
+  '--disable-domain-reliability',
+  '--disable-extensions',
+  '--disable-features=AudioServiceOutOfProcess',
+  '--disable-hang-monitor',
+  '--disable-ipc-flooding-protection',
+  '--disable-notifications',
+  '--disable-offer-store-unmasked-wallet-cards',
+  '--disable-popup-blocking',
+  '--disable-print-preview',
+  '--disable-prompt-on-repost',
+  '--disable-renderer-backgrounding',
+  '--disable-setuid-sandbox',
+  '--disable-speech-api',
+  '--disable-sync',
+  '--hide-scrollbars',
+  '--ignore-gpu-blacklist',
+  '--metrics-recording-only',
+  '--mute-audio',
+  '--no-default-browser-check',
+  '--no-first-run',
+  '--no-pings',
+  '--no-sandbox',
+  '--no-zygote',
+  '--password-store=basic',
+  '--use-gl=swiftshader',
+  '--use-mock-keychain',
+];
+
+// only 'max' is required, the others are optional, but MAY be
+// required if certain other fields are set.
+const cache_options = {
+  max: 64,
+  ttl: 86400000, // 1 day in ms
+  allowStale: true, 
+  updateAgeOnGet: true,
+  updateAgeOnHas: false,
+  updateRecencyOnHas: true,
+}
+
+const urlImageIsAccessible = async (url) => {
+  const correctedUrls = getUrls(url);
+  if (isBase64(url, { allowMime: true })) {
+    return true;
+  }
+  if (correctedUrls.size !== 0) {
+    const urlResponse = await request(correctedUrls.values().next().value);
+    const contentType = urlResponse.headers["content-type"];
+    return new RegExp("image/*").test(contentType);
+  }
+};
+
+const getImg = async (page, uri) => {
+  const img = await page.evaluate(async () => {
+    const ogImg = document.querySelector('meta[property="og:image"]');
+    if (
+      ogImg != null &&
+      ogImg.content.length > 0 &&
+      (await urlImageIsAccessible(ogImg.content))
+    ) {
+      return ogImg.content;
+    }
+    const imgRelLink = document.querySelector('link[rel="image_src"]');
+    if (
+      imgRelLink != null &&
+      imgRelLink.href.length > 0 &&
+      (await urlImageIsAccessible(imgRelLink.href))
+    ) {
+      return imgRelLink.href;
+    }
+    const twitterImg = document.querySelector('meta[name="twitter:image"]');
+    if (
+      twitterImg != null &&
+      twitterImg.content.length > 0 &&
+      (await urlImageIsAccessible(twitterImg.content))
+    ) {
+      return twitterImg.content;
+    }
+
+    let imgs = Array.from(document.getElementsByTagName("img"));
+    if (imgs.length > 0) {
+      imgs = imgs.filter((img) => {
+        let addImg = true;
+        if (img.naturalWidth > img.naturalHeight) {
+          if (img.naturalWidth / img.naturalHeight > 3) {
+            addImg = false;
+          }
+        } else {
+          if (img.naturalHeight / img.naturalWidth > 3) {
+            addImg = false;
+          }
+        }
+        if (img.naturalHeight <= 50 || img.naturalWidth <= 50) {
+          addImg = false;
+        }
+        return addImg;
+      });
+      if (imgs.length > 0) {
+        imgs.forEach((img) =>
+          img.src.indexOf("//") === -1
+            ? (img.src = `${new URL(uri).origin}/${img.src}`)
+            : img.src
+        );
+        return imgs[0].src;
       }
+    }
+    return null;
+  });
+  return img;
+};
+
+const getTitle = async (page) => {
+  const title = await page.evaluate(() => {
+    const ogTitle = document.querySelector('meta[property="og:title"]');
+    if (ogTitle != null && ogTitle.content.length > 0) {
+      return ogTitle.content;
+    }
+    const twitterTitle = document.querySelector('meta[name="twitter:title"]');
+    if (twitterTitle != null && twitterTitle.content.length > 0) {
+      return twitterTitle.content;
+    }
+    const docTitle = document.title;
+    if (docTitle != null && docTitle.length > 0) {
+      return docTitle;
+    }
+    const h1El = document.querySelector("h1");
+    const h1 = h1El ? h1El.innerHTML : null;
+    if (h1 != null && h1.length > 0) {
+      return h1;
+    }
+    const h2El = document.querySelector("h2");
+    const h2 = h2El ? h2El.innerHTML : null;
+    if (h2 != null && h2.length > 0) {
+      return h2;
+    }
+    return null;
+  });
+  return title;
+};
+
+const getDescription = async (page) => {
+  const description = await page.evaluate(() => {
+    const ogDescription = document.querySelector(
+      'meta[property="og:description"]'
+    );
+    if (ogDescription != null && ogDescription.content.length > 0) {
+      return ogDescription.content;
+    }
+    const twitterDescription = document.querySelector(
+      'meta[name="twitter:description"]'
+    );
+    if (twitterDescription != null && twitterDescription.content.length > 0) {
+      return twitterDescription.content;
+    }
+    const metaDescription = document.querySelector('meta[name="description"]');
+    if (metaDescription != null && metaDescription.content.length > 0) {
+      return metaDescription.content;
+    }
+    let paragraphs = document.querySelectorAll("p");
+    let fstVisibleParagraph = null;
+    for (let i = 0; i < paragraphs.length; i++) {
+      if (
+        // if object is visible in dom
+        paragraphs[i].offsetParent !== null &&
+        !paragraphs[i].childElementCount != 0
+      ) {
+        fstVisibleParagraph = paragraphs[i].textContent;
+        break;
+      }
+    }
+    return fstVisibleParagraph;
+  });
+  return description;
+};
+
+const getDomainName = async (page, uri) => {
+  const domainName = await page.evaluate(() => {
+    const canonicalLink = document.querySelector("link[rel=canonical]");
+    if (canonicalLink != null && canonicalLink.href.length > 0) {
+      return canonicalLink.href;
+    }
+    const ogUrlMeta = document.querySelector('meta[property="og:url"]');
+    if (ogUrlMeta != null && ogUrlMeta.content.length > 0) {
+      return ogUrlMeta.content;
+    }
+    return null;
+  });
+  return domainName != null
+    ? new URL(domainName).hostname.replace("www.", "")
+    : new URL(uri).hostname.replace("www.", "");
+};
+
+const cache = new LRU(cache_options)
+let browser; // browser object
+
+async function launch () {
+  puppeteer.use(pluginStealth());
+
+  const params = {
+    headless: true,
+    args: minimal_args,
+  };
+
+  browser = await puppeteer.launch(params);
+}
+
+async function getPreview (
+  uri,
+  puppeteerAgent = "facebookexternalhit/1.1 (+http://www.facebook.com/externalhit_uatext.php)",
+) {
+  if (browser === 'undefined') {
+    return;
+  }
+
+  const obj = {};
+  const page = await browser.newPage();
+
+  try {
+    page.setUserAgent(puppeteerAgent);
+
+    await page.goto(uri);
+    await page.exposeFunction("request", request);
+    await page.exposeFunction("urlImageIsAccessible", urlImageIsAccessible);
+
+    obj.title = await getTitle(page);
+    obj.description = await getDescription(page);
+    obj.domain = await getDomainName(page, uri);
+    obj.img = await getImg(page, uri);
+  }
+  catch (error) {
+    console.error(error);
+  }
+  finally {
+    await page.close();
+  }
+  
+  return obj;
+}
+
+app.get('/preview', async (req, res) => {
+  const uri = req.query.url;
+  console.log(`Generating preview for --> ${uri}`);
+
+  if (cache.has(uri)) {
+    console.log(`Success! Cache found preview for --> ${uri}`)
+    res.send(cache.get(uri));
+    return;
+  }
+
+  try {
+      const previewData = await getPreview(uri);
+
+      if (previewData === 'undefined') {
+        console.error(`Internal server error when creating preview for --> ${uri}`);
+        res.sendStatus(500);
+        return;
+      }
+
+      console.log(`Success! Preview generated for --> ${uri}`)
+      res.json(previewData);
+      cache.set(uri, previewData);
+    } catch (error) {
+        console.log(error);
+        res.send(error);
+    }
 });
 
 app.listen(port, () => {
+  launch();
   console.log(`Link preview server listening on port ${port}`)
 });
+
+process.on('SIGTERM', () => {
+  console.log('Link preview server exiting');
+  browser.close();
+  server.close(() => {
+    console.log('Link preview server closed');
+  })
+})
